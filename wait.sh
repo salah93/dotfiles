7600bb4f42b4204f54d66fa01da374d4ad0a7e08
Salah Ahmed <salahs.email@pm.me>
2020-05-04 05:18:19 -0400

    add scripts to render rst files

---
+++ wait.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ue
+
+# source virtualenv
+source ~/.virtualenvs/env/bin/activate
+OUT=/tmp/html
+DIR=$1
+
+mkdir -p $OUT
+pushd $OUT
+browser-sync start --server --files "*html" &
+popd
+
+inotifywait -m -e modify --format %f $DIR | while read filename
+do
+    if [ ${filename##*.} = 'rst' ]
+    then
+        rst2html --source $filename --out $OUT
+    fi
+done
