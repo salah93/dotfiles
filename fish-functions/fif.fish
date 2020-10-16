cf9cfdebb5134abde941c9d701bc55b271822608
Salah Ahmed <sahmed@resy.com>
2020-10-16 12:03:33 -0400

    remove exit

--- fish-functions/fif.fish
+++ fish-functions/fif.fish
@@ -1,9 +1,7 @@
 function fif
     if test (count $argv) != 1
         echo need argument
-        exit 1
     else
         vim (rg --files-with-matches --no-messages "$argv[1]" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$argv[1]' || rg --ignore-case --pretty --context 10 '$argv[1]' {}")
-        exit 0
     end
 end
