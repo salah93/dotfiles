48c4eb57c941645c636f262caaaadd0dab370fbc
Salah Ahmed <salahs.email@pm.me>
2021-01-30 14:18:36 -0500

    add askpass env

--- fish/.config/fish/conf.d/ssh_agent.fish
+++ fish/.config/fish/conf.d/ssh_agent.fish
@@ -2,8 +2,12 @@
 # include the line X-GNOME-Autostart-enabled=false
 # then restart for it to take effect
 #
+if test -e /usr/libexec/openssh/gnome-ssh-askpass
+    set -x SSH_ASKPASS /usr/libexec/openssh/gnome-ssh-askpass
+end
+
 if test -z (pgrep ssh-agent)
-  eval (ssh-agent -t 1800 -c)
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    eval (ssh-agent -t 1800 -c)
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
 end
