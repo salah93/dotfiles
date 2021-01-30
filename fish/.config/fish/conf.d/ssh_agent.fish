0b6144e33a0528a293063b5bd42e3355cf960581
Salah Ahmed <salahs.email@pm.me>
2021-01-30 13:56:27 -0500

    add comment for ssh-agent config

--- fish/.config/fish/conf.d/ssh_agent.fish
+++ fish/.config/fish/conf.d/ssh_agent.fish
@@ -1,3 +1,7 @@
+# on gnome edit this file /etc/xdg/autostart/gnome-keyring-ssh.desktop
+# include the line X-GNOME-Autostart-enabled=false
+# then restart for it to take effect
+#
 if test -z (pgrep ssh-agent)
   eval (ssh-agent -t 1800 -c)
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
