1b5294c7f9729086391ca4eecb231dc90cc990bf
Salah Ahmed <salahs.email@pm.me>
2021-01-30 10:42:49 -0500

    remove duplicate line

--- fish/.config/fish/conf.d/ssh_agent.fish
+++ fish/.config/fish/conf.d/ssh_agent.fish
@@ -2,5 +2,4 @@ if test -z (pgrep ssh-agent)
   eval (ssh-agent -t 1800 -c)
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
 end
