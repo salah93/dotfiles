8a4b8c1252dbd73bfdd3966570d48edda762568a
Salah Ahmed <salahs.email@pm.me>
2021-01-30 21:37:01 -0500

    update comment location

--- fish/.config/fish/conf.d/ssh_agent.fish
+++ fish/.config/fish/conf.d/ssh_agent.fish
@@ -1,7 +1,3 @@
-# on gnome edit this file /etc/xdg/autostart/gnome-keyring-ssh.desktop
-# include the line X-GNOME-Autostart-enabled=false
-# then restart for it to take effect
-#
 if test -e /usr/libexec/openssh/ssh-askpass
     set -Ux SSH_ASKPASS /usr/libexec/openssh/ssh-askpass
 end
--- fish/.config/fish/functions/start-ssh-agent.fish
+++ fish/.config/fish/functions/start-ssh-agent.fish
@@ -1,3 +1,7 @@
+# on gnome edit this file /etc/xdg/autostart/gnome-keyring-ssh.desktop
+# include the line X-GNOME-Autostart-enabled=false
+# then restart for it to take effect
+#
 function start-ssh-agent
     eval (ssh-agent -t 1800 -c)
     set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
