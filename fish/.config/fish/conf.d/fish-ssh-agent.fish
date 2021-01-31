295207f12cd198f2027e8081d2cd1146720ae912
Salah Ahmed <salahs.email@pm.me>
2021-01-31 17:31:39 -0500

    source ssh env if exists

--- fish/.config/fish/conf.d/fish-ssh-agent.fish
+++ fish/.config/fish/conf.d/fish-ssh-agent.fish
@@ -5,6 +5,8 @@ else if type -q ssh-askpass
     set -x SSH_ASKPASS (which ssh-askpass)
 end
 
-if test -z "$SSH_ENV"
+if test -z $SSH_ENV
     set -xg SSH_ENV $HOME/.ssh/environment
+else if test -f $SSH_ENV
+    source $SSH_ENV
 end
