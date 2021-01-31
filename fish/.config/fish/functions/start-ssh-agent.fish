96fb13443a5b663cddb474c43f9cff52a1b9e1f8
Salah Ahmed <salahs.email@pm.me>
2021-01-30 21:55:25 -0500

    remove start ssh-agent file
    its better to go with fish-ssh-agent
    fisher install danhper/fish-ssh-agent

--- fish/.config/fish/functions/start-ssh-agent.fish
+++
@@ -1,9 +0,0 @@
-# on gnome edit this file /etc/xdg/autostart/gnome-keyring-ssh.desktop
-# include the line X-GNOME-Autostart-enabled=false
-# then restart for it to take effect
-#
-function start-ssh-agent
-    eval (ssh-agent -t 1800 -c)
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-end
