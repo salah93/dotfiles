a772d68993b81f26d0233b50a3da5bfd605d1977
Salah Ahmed <salahs.email@pm.me>
2021-02-18 09:49:06 -0500

    update variable in ssh-agent check

--- fish/.config/fish/conf.d/fish-ssh-agent.fish
+++ fish/.config/fish/conf.d/fish-ssh-agent.fish
@@ -12,7 +12,7 @@ pgrep -fq "ssh-agent -t 1800 -c"
 set -l ssh_agent_is_running $status
 
 if test -f $SSH_ENV
-   and test $status -eq 0
+   and test $ssh_agent_is_running -eq 0
     source $SSH_ENV > /dev/null 2>&1
 else
     kill-ssh-agent
