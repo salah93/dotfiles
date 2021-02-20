40343a1694bf48027cace0aa1b4e991d17c93178
Salah Ahmed <salahs.email@pm.me>
2021-02-20 00:11:39 -0500

    fix ssh environment file

--- fish/.config/fish/functions/start-ssh-agent.fish
+++ fish/.config/fish/functions/start-ssh-agent.fish
@@ -5,8 +5,8 @@ function start-ssh-agent
     set -gx DISPLAY :0
     eval (ssh-agent -t 1800 -c)
     # WARNING - setting display in startup results in login loop in gnome!!
-    echo set -gx DISPLAY :0 >> $SSH_ENV
-    echo set -gx SSH_AUTH_SOCK $SSH_AUTH_SOCK > $SSH_ENV
+    echo set -gx DISPLAY :0 > $SSH_ENV
+    echo set -gx SSH_AUTH_SOCK $SSH_AUTH_SOCK >> $SSH_ENV
     echo set -gx SSH_AGENT_PID $SSH_AGENT_PID >> $SSH_ENV
     chmod 600 $SSH_ENV
     source $SSH_ENV > /dev/null 2>&1
