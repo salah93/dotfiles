17213d26664310bedbc444807ec2bdfef4f3c0e5
salah ahmed <salahs.email@pm.me>
2025-02-21 07:46:48 -0500

    show documentation in python

--- neovim/.config/nvim/lua/plugins/coc.lua
+++ neovim/.config/nvim/lua/plugins/coc.lua
@@ -13,6 +13,19 @@ keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_spa
 keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 
 
+-- Use K to show documentation in preview window
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+
 
 -- Formatting selected code
 --keyset("x", "<leader>f", "<Plug>(coc-format-selected)", {silent = true})
