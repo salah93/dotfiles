58f5faacfa44ff1cda1de4489c11f962399d118b
salah ahmed <salahs.email@pm.me>
2025-06-14 14:33:44 -0400

    call snippets from init.lua

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -66,3 +66,4 @@ vim.g.tagbar_type_go = {
 
 -- Disable tmux navigator when zooming the Vim pane
 vim.g.tmux_navigator_disable_when_zoomed = 1
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/lua/snippets/"})
